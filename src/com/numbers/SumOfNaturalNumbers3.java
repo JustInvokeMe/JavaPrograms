60ea020b73d69ac6b8926d171a606d2e4142a3cb
Yesod <73208581+YesodHeaven@users.noreply.github.com>
2020-10-22 15:17:09 +0200

    Package com.numbers

---
+++ src/com/numbers/SumOfNaturalNumbers3.java
@@ -0,0 +1,32 @@
+package com.numbers;
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers3
+{
+    public static void main(String[] args)
+    {
+        int num, count, total = 0;
+
+        System.out.println("Enter the value of n:");
+
+        // Scanner is used for reading user input.
+
+        Scanner scan = new Scanner(System.in);
+
+        // nextInt() method reads integer entered by user
+
+        num = scan.nextInt();
+
+        // Closing scanner after use.
+
+        scan.close();
+
+        for (count = 1; count <= num; count ++)
+        {
+            total = total + count;
+        }
+
+        System.out.println("Sum of first "+ num +" natural numbers is: " + total);
+    }
+}

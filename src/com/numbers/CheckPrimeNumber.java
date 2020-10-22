a2c5dd0bd4d0686f32f262fe714fb2f96f46b710
Yesod <73208581+YesodHeaven@users.noreply.github.com>
2020-10-22 12:03:15 +0200

    Package com.numbers

---
+++ src/com/numbers/CheckPrimeNumber.java
@@ -0,0 +1,35 @@
+package com.numbers;
+
+import java.util.Scanner;
+
+public class CheckPrimeNumber {
+
+    public static void main(String[] args) {
+
+        int temp;
+        boolean isPrime = true;
+        Scanner scan = new Scanner(System.in);
+        System.out.println(" Enter any number : ");
+
+        // Capture the input in an integer.
+
+        int num = scan.nextInt();
+        scan.close();
+        for ( int i = 2; i <= num / 2; i ++) {
+
+            temp = num % i;
+            if ( temp == 0 ) {
+
+                isPrime=false;
+                break;
+            }
+        }
+
+        // If isPrime is true then the number is prime else not
+
+        if ( isPrime )
+            System.out.println( num + " is a Prime number" );
+        else
+            System.out.println( num + " is not a Prime Number ");
+    }
+}

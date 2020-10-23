b5547443f3822155619df505bf789b116b448007
Yesod <73208581+YesodHeaven@users.noreply.github.com>
2020-10-23 09:33:51 +0200

    Package com.numbers

---
+++ src/com/numbers/EvenNumbers.java
@@ -0,0 +1,21 @@
+package com.numbers;
+
+public class EvenNumbers
+{
+    public static void main(String[] args)
+    {
+        int n = 100;
+
+        System.out.print("Even Numbers from 1 to to " + n + " are: ");
+
+        for (int i = 1; i <= n; i ++)
+        {
+            // If n % 2 == 0 it means its an even number
+
+            if (i % 2 == 0)
+            {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}

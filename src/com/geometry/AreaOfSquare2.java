af771686e8fbb95872df1b93a83336e33aa83750
Yesod <73208581+YesodHeaven@users.noreply.github.com>
2020-10-24 14:09:21 +0200

    Package com.geometry

---
+++ src/com/geometry/AreaOfSquare2.java
@@ -0,0 +1,17 @@
+package com.geometry;
+
+public class AreaOfSquare2
+{
+    public static void main(String[] args)
+    {
+        // Value specified in the program itself.
+
+        double side = 4.5;
+
+        // Area of Square = side * side.
+
+        double area = side * side;
+
+        System.out.println("Area of Square is: " + area);
+    }
+}

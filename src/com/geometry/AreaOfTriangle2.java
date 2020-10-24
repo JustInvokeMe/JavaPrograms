981d8027ab80ed6434d2aade41e299b1cc6297f8
Yesod <73208581+YesodHeaven@users.noreply.github.com>
2020-10-24 15:21:41 +0200

    Package com.geometry

---
+++ src/com/geometry/AreaOfTriangle2.java
@@ -0,0 +1,13 @@
+package com.geometry;
+
+public class AreaOfTriangle2
+{
+    public static void main(String[] args)
+    {
+        double base = 20.0;
+        double height = 110.5;
+        double area = (base * height) / 2;
+
+        System.out.println("Area of Triangle is: " + area);
+    }
+}

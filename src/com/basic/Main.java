c8a62614845e09490421cc30a9952b3d68b42bc4
Yesod <73208581+YesodHeaven@users.noreply.github.com>
2020-10-24 17:45:27 +0200

    Added GUI
    GUI for the main class.

--- src/com/basic/Main.java
+++ src/com/basic/Main.java
@@ -1,8 +1,43 @@
 package com.basic;
 
-public class Main {
+import javax.swing.*;
 
-    public static void main(String[] args) {
-	System.out.println("This is my first java's program!");
+public class Main extends JFrame
+{
+    final JLabel label1 = new JLabel("Text");
+    final JLabel label2 = new JLabel("Text");
+
+    public Main()
+    {
+        super("JavaPrograms");
+        setSize(300, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel pane = new JPanel();
+        pane.add(label1);
+        pane.add(label2);
+        label1.setText("★ In this build there are various Java programs");
+        label2.setText(" organized by package. Make good use of it !");
+        add(pane);
+        setVisible(true);
+    }
+    
+    public static void setLookAndFell()
+    {
+        try
+        {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        }
+
+        catch (Exception exc)
+        {
+            // Ignore error.
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        setLookAndFell();
+        @SuppressWarnings("unused")
+        Main main = new Main();
     }
 }

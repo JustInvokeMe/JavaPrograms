144cb5b83daeebea40c840644149185f4881e0e3
Yesod <73208581+YesodHeaven@users.noreply.github.com>
2020-11-03 21:45:36 +0100

    Package com.other

---
+++ src/com/other/SumOfMultipleNumbers.java
@@ -0,0 +1,38 @@
+package com.other;
+
+@SuppressWarnings("SameParameterValue")
+
+public class SumOfMultipleNumbers
+{
+    int add (int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    int add (int num1, int num2, int num3)
+    {
+        return num1 + num2 + num3;
+    }
+
+    int add (int num1, int num2, int num3 , int num4)
+    {
+        return num1 + num2 + num3 + num4;
+    }
+
+    public static void main(String[] args)
+    {
+        SumOfMultipleNumbers obj = new SumOfMultipleNumbers();
+
+        // This will call the first add method.
+
+        System.out.println("Sum of two numbers: " + obj.add(10, 20));
+
+        // This will call second add method.
+
+        System.out.println("Sum of three numbers: " + obj.add(10, 20, 30));
+
+        // this will call third add method.
+
+        System.out.println("Sum of four numbers: " + obj.add(1, 2, 3, 4));
+    }
+}

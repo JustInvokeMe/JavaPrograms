27210d99b1f5fcc783be1c368265af04191a2ef5
Yesod <73208581+YesodHeaven@users.noreply.github.com>
2020-11-03 20:48:15 +0100

    Package com.other

---
+++ src/com/other/FindFactorial2.java
@@ -0,0 +1,22 @@
+package com.other;
+
+public class FindFactorial2
+{
+    public static void main(String[] args)
+    {
+        int number = 5;
+
+        long fact = 1;
+
+        int i = 1;
+
+        while (i <= number)
+        {
+            fact = fact * i;
+
+            i++;
+        }
+
+        System.out.println("Factorial of " + number + " is: " + fact);
+    }
+}

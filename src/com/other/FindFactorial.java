86c5b08b4d9d87921f79591b3e22a6cd4730bd99
Yesod <73208581+YesodHeaven@users.noreply.github.com>
2020-11-03 20:43:56 +0100

    Package com.other

---
+++ src/com/other/FindFactorial.java
@@ -0,0 +1,20 @@
+package com.other;
+
+public class FindFactorial 
+{
+    public static void main(String[] args)
+    {
+        // We will find the factorial of this number.
+
+        int number = 5;
+
+        long fact = 1;
+
+        for (int i = 1; i <= number; i++)
+        {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial of " + number + " is: " + fact);
+    }
+}
